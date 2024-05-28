b5f33b12a68d5a0ca6335de35ced873365585ba7
kottefadyl <kottekevin01@gmail.com>
2024-05-28 14:25:44 +0200

    commit terminal

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe())
+    // Enable CORS for all origins
+    app.enableCors({
+      origin: '*', // Allow all origins
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true, // Allow cookies to be sent
+    });
   
+  app.useGlobalPipes(new ValidationPipe())
 }
 bootstrap();
